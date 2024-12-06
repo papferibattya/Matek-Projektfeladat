99f97cc9ba6d5cb815b87c76762af35ff074f58b
Papayayoo <93657843+Papayayoo@users.noreply.github.com>
2024-12-06 12:20:42 +0100

    BenceResze

---
+++ Kalapacsvetes/Kalapacsvetes/Sportolo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalapacsvetes
+{
+    internal class Sportolo
+    {
+        public int Helyezes { get; set; }
+        public double Eredmeny { get; set; }
+        public string Nev { get; set; }
+        public string Orszagkod { get; set; }
+        public string Helyszin { get; set; }
+        public DateTime Datum { get; set; }
+
+        public Sportolo(int helyezes, double eredmeny, string nev, string orszagkod, string helyszin, DateTime datum)
+        {
+            Helyezes = helyezes;
+            Eredmeny = eredmeny;
+            Nev = nev;
+            Orszagkod = orszagkod;
+            Helyszin = helyszin;
+            Datum = datum;
+        }
+
+        public override string ToString()
+        {
+            return $"{Helyezes}. {Nev} ({Orszagkod}), {Eredmeny}m, {Helyszin}, {Datum:yyyy.MM.dd}";
+        }
+    }
+}
